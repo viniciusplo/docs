0ac3a303ca4c94c1d79a7ad572ec7f6e3275144b
Vinícius Oliveira <vinicius.oliveira@datah.com.br>
2021-03-15 13:17:34 -0300

    ajust theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,11 +26,12 @@ release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
-
+import sphinx_rtd_theme
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -48,7 +49,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
